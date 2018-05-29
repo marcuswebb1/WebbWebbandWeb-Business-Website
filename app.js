1819f1a6e9a6528676743fe5e77c90f68cd2d9c2
Marcus Webb <marcuswebb1211@gmail.com>
2018-05-28 22:03:23 -0500

    Update the app.js file

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ $('.hero').delay(1000).hide().delay(1000).fadeIn(1000).slideDown(1000);
 $('.services').delay(1000).hide().delay(2000).fadeIn(1000).slideDown(1000);
 $('.gallery').delay(1000).hide().delay(3000).fadeIn(1000);
 $('.about').delay(1000).hide().delay(4000).fadeIn(1000);
+$('.centered').delay(1000).hide().delay(4000).fadeIn(1000);
 $('.contact').delay(1000).hide().delay(5000).fadeIn(1000);
 $('.footer').delay(1000).hide().delay(6000).fadeIn(1000);
